dff10e62ec00aebab953f5a7a8d624f34e43e261
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-09 01:16:30 +0530

    Use random function to know gambler won or lost

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -2,6 +2,22 @@ echo "Welcome to gambling simulation problem"
 
 Stake=100
 Bet=1
-
+Win=1
+Loss=0
 echo "Initial Stake $Stake"
 
+function result()
+{
+        random=$(( RANDOM%2 ))
+
+        if [ $random -eq 1 ]
+        then
+                Stake=$(( Stake+1 ))
+        else
+                Stake=$(( Stake-1 ))
+        fi
+}
+
+result
+
+echo $Stake
