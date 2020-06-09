4c6ccaedf0a30c4ef8484cf9619ef011aeca2040
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-09 17:50:42 +0530

    ADDED:isContinuePlaying function and put the condition of continue playing or not

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -1,76 +1,95 @@
 echo "Welcome to gambling simulation problem"
 
-stake=100
-Bet=1
-Win=1
-Loss=0
+#constants
+STAKE=100
+NUM_OFDAYS=30
+WIN=1
+LOSS=0
+BET=1
+IS_VALID=true
+LAST_LOSSAMOUNT=0
 
-echo "Initial Stake $stake"
+echo "Initial Stake $STAKE"
+echo "enter percent of the stake"
+read UserPercent
 
-stakePercentAmount=$(( 50 * $stake/100 ))
-max_win=$(( $stakePercentAmount+$stake ))
-max_loss=$(( $stake-$stakePercentAmount ))
-numOfDays=30
+#variables
+stakePercentAmount=$(( $UserPercent*STAKE/100 ))
+maxWin=$(( $stakePercentAmount+$STAKE ))
+maxLoss=$(( $STAKE-$stakePercentAmount ))
 totalWinOrLoss=0
 daysWin=0
 daysLoss=0
 
-declare -A fullDay
-declare -A maxMinDay
+declare -A day
+declare -A month
 
-function getresult()
+function getdailyBet()
 {
-	dayStake=$stake
-	while [ $dayStake -lt $max_win ] && [ $dayStake -gt $max_loss ]
-		do
-        random=$(( RANDOM%2 ))
-
-        if [ $random -eq 1 ]
-        then
-				dayStake=$(( $dayStake+$Bet ))
-        else
-				dayStake=$(( $dayStake-$Bet ))
-        fi
-		done
+   dayStake=$STAKE
+   while [ $dayStake -lt $maxWin ] && [ $dayStake -gt $maxLoss ]
+   do
+     random=$(( RANDOM % 2 ))
+     if [ $random -eq 1 ]
+     then
+        dayStake=$(( $dayStake+$BET ))
+     else
+        dayStake=$(( $dayStake-$BET ))
+     fi
+   done
 }
 
-	for (( day=0; day<$numOfDays; day++ ))
-		do
-        getresult
+function getmonthBet(){
 
-			if [ $dayStake -eq $max_loss ]
-				then
-					totalWinOrloss=$(( $totalWinOrloss-$stakePercentAmount ))
-					fullDay["Day $day"]=-$stakePercentAmount
-					maxMinDay["Day $day"]=$totalWinOrloss
-               (( daysLoss++ ))
-				else
-					totalWinOrloss=$(( $totalWinOrloss+$stakePercentAmount ))
-					fullDay["Day $day"]=$stakePercentAmount
-					maxMinDay["Day $day"]=$totalWinOrloss
-					(( daysWin++ ))
-			 fi
-		done
+   for (( day=1; day<=$NUM_OFDAYS; day++ ))
+   do
+   getdailyBet
+      if [ $dayStake -eq $maxLoss ]
+      then
+         totalWinOrLoss=$(( $totalWinOrLoss - $stakePercentAmount ))
+         day["Day $day"]=-$stakePercentAmount
+         month["Day $day"]=$totalWinOrLoss
+         ((daysLoss++))
+      else
+         totalWinOrLoss=$(( $totalWinOrLoss + $stakePercentAmount ))
+         day["Day $day"]=$stakePercentAmount
+         month["Day $day"]=$totalWinOrLoss
+         ((daysWin++))
+      fi
+   done
 
-echo "Total Won orloss : $totalWinOrLoss"
-echo "No. of days Win $daysWin by $(($daysWin*$stakePercentAmount))" 
-echo "No. of days Loss $daysLoss by  $(($daysLoss*$stakePercentAmount))"
-echo "${!maxMinDay[@]} : ${maxMinDay[@]}"
+   echo "Total Won/loss : $totalWinOrLoss"
+   echo "No. of days Win $daysWin by $(($daysWin*$stakePercentAmount))" 
+   echo "No. of days Loss $daysLoss by  $(($daysLoss*$stakePercentAmount))"
+   echo "${!month[@]} : ${month[@]}"
 
-luckiestDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | head -1 )
-unluckiestDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | tail -1 )
-
-
-	for data in "${!maxMinDay[@]}"
-     	do
-		if [[ ${maxMinDay[$data]} -eq $luckiestDay ]]
-		then
-		echo "LUCKIEST DAY-" $data
-		elif [[ ${maxMinDay[$data]} -eq $unluckiestDay ]]
-		then
-		echo "UNLUCKIEST DAY-" $data
-		fi
-     	done
+   luckiestDay=$( printf "%s\n" ${month[@]} | sort -nr | head -1 )
+   unluckiestDay=$( printf "%s\n" ${month[@]} | sort -nr | tail -1 )
 
 
+   for data in "${!month[@]}"
+   do
+      if [[ ${month[$data]} -eq $luckiestDay ]]
+      then
+         echo "LUCKIEST DAY- $data $luckiestDay"
+      elif [[ ${month[$data]} -eq $unluckiestDay ]]
+      then
+         echo "UNLUCKIEST DAY- $data $unluckiestDay"
+      fi
+   done
+}
 
+function isContinuePlaying(){
+while [ $IS_VALID ]
+do
+   getmonthBet
+   if [ $totalWinOrLoss -lt $LAST_LOSSAMOUNT ]
+   then
+      echo "you lost, can't paly"
+      break
+   else
+      echo "you won $totalWinOrLoss, continue playing"
+   fi
+done
+}
+ isContinuePlaying
