59d91f1e74e2b9af32d5bb09edb289df3cdbd3b8
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-09 13:31:34 +0530

    find the luckiest and unluckiest day for winning or loosing

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -5,7 +5,7 @@ Bet=1
 Win=1
 Loss=0
 
-echo "Initial Stake $Stake"
+echo "Initial Stake $stake"
 
 stakePercentAmount=$(( 50 * $stake/100 ))
 max_win=$(( $stakePercentAmount+$stake ))
@@ -16,6 +16,7 @@ daysWin=0
 daysLoss=0
 
 declare -A fullDay
+declare -A maxMinDay
 
 function getresult()
 {
@@ -41,17 +42,35 @@ function getresult()
 				then
 					totalWinOrloss=$(( $totalWinOrloss-$stakePercentAmount ))
 					fullDay["Day $day"]=-$stakePercentAmount
+					maxMinDay["Day $day"]=$totalWinOrloss
                (( daysLoss++ ))
 				else
 					totalWinOrloss=$(( $totalWinOrloss+$stakePercentAmount ))
 					fullDay["Day $day"]=$stakePercentAmount
+					maxMinDay["Day $day"]=$totalWinOrloss
 					(( daysWin++ ))
 			 fi
 		done
 
-echo "No. of days Win $daysWin by $(($daysWin*$stakePercentAmount))"
-echo "No. of days Loss $daysLoss by $(($daysLoss*$stakePercentAmount))"
+echo "Total Won orloss : $totalWinOrLoss"
+echo "No. of days Win $daysWin by $(($daysWin*$stakePercentAmount))" 
+echo "No. of days Loss $daysLoss by  $(($daysLoss*$stakePercentAmount))"
+echo "${!maxMinDay[@]} : ${maxMinDay[@]}"
 
+luckiestDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | head -1 )
+unluckiestDay=$( printf "%s\n" ${maxMinDay[@]} | sort -nr | tail -1 )
+
+
+	for data in "${!maxMinDay[@]}"
+     	do
+		if [[ ${maxMinDay[$data]} -eq $luckiestDay ]]
+		then
+		echo "LUCKIEST DAY-" $data
+		elif [[ ${maxMinDay[$data]} -eq $unluckiestDay ]]
+		then
+		echo "UNLUCKIEST DAY-" $data
+		fi
+     	done
 
 
 
